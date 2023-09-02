0cf2d7bab6245b1a28560b01a177ddb3832a010e
Yared04 <yaredt29@gmail.com>
2023-09-02 13:02:58 +0300

    Changed js to tsx

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import "@/styles/globals.css";
+import { AppProps } from "next/app";
+import { createContext, useState } from "react";
+
+export const AppContext = createContext({
+  state: {
+    curUser: { id: "", username: "" },
+  },
+  setState: (state) => {},
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [state, setState] = useState({
+    curUser: { id: "", username: "" },
+  });
+  return (
+    <AppContext.Provider value={{ state, setState }}>
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  );
+}
