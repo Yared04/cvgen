edaa7ce7ae9b79b8e30882b7512656b65864b34c
Yared04 <yaredt29@gmail.com>
2023-09-02 14:20:12 +0300

    added the basic informations component

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+interface UserProps {
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+  photo: string;
+}
+
+export const UserCard = ({ name, email, phone, address, photo }: UserProps) => {
+  return (
+    <div className="border-2 p-8 rounded-md">
+      <h1 className="font-bold text-xl align-start">Basic Information</h1>
+      <div className="flex gap-2 my-4">
+        <img
+          className="rounded-full w-16 h-16"
+          src={photo}
+          alt="profile picture"
+        />
+        <h2 className="m-auto font-semibold">{name}</h2>
+      </div>
+      <span className="flex gap-2">
+        <img src="./address.svg" alt="address" />
+        <p className="font-semibold">Address</p>
+      </span>
+
+      <p className="text-text-secondary text-sm pl-7 mb-4">{address}</p>
+      <span className="flex gap-2">
+        <img src="./email.svg" alt="email" />
+        <p className="font-semibold">Email</p>
+      </span>
+      <p className="text-text-secondary text-sm pl-7 mb-4">{email}</p>
+      <span className="flex gap-2">
+        <img src="./phone.svg" alt="phone" />
+        <p className="font-semibold">Phone Number</p>
+      </span>
+      <p className="text-text-secondary text-sm pl-7 mb-4">{phone}</p>
+    </div>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import { AppProps } from "next/app";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const AppContext = createContext({
   state: {
@@ -13,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
   const [state, setState] = useState({
     curUser: { id: "", username: "" },
   });
+
   return (
     <AppContext.Provider value={{ state, setState }}>
       <Component {...pageProps} />
