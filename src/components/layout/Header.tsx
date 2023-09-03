2405e01c5abd232580da396140cb12981b991208
natnael16 <natnaeldenbi1@gmail.com>
2023-09-03 11:51:38 +0300

    Add user profile on the header.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,26 @@
+import supabase from "@/utils/supabaseClient";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState , useEffect} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+
 
 const Header: React.FC = () => {
+  console.log(supabase.auth.getSession())
+  const [hasSession,setHasSession] = useState(false)
+  useEffect(() => {
+    const getCurUser = async () => {
+
+      var session = await supabase.auth.getSession();
+      console.log(session)
+      setHasSession(session.data.session.access_token != null && session.data.session.access_token != '');
+    };
+
+    getCurUser();
+  }, [])
+
   return (
     <div className="flex w-full py-4 px-2 md:px-16 text-white bg-primary items-center justify-between">
       <Link href="/">
@@ -23,8 +41,18 @@ const Header: React.FC = () => {
         <Link href={"/#team"}>Team</Link>
       </div>
       <div></div>
+      
+     {!hasSession ? <SignInComponent/> : <ProfileComponent/>}
 
-      <div className="flex text-sm items-center space-x-2">
+    </div>
+  );
+};
+
+export default Header;
+
+
+const SignInComponent = ({}) => {
+  return (<div className="flex text-sm items-center space-x-2">
         <Link href={"/login"}>
           <div className="flex p-2">
             <p>Sign In</p>
@@ -35,9 +63,21 @@ const Header: React.FC = () => {
             <p>Sign Up</p>
           </div>
         </Link>
+      </div>);}
+
+
+const ProfileComponent = () => {
+
+  return (
+    <Link href={'/profilebuild'}>
+    <div className="flex items-center justify-center">
+      <div className="text-white cursor-pointer">
+        <FontAwesomeIcon icon={faUser} className="text-white w-6 h-6 text-3xl" />
+        <span className="text-white ml-2">Profile</span>
       </div>
     </div>
+    </Link>
+    
   );
 };
 
-export default Header;
