f743ae219b6de7129b0d33bd442529ec17b092e3
zerabruckmuluneh <zerabruck@a2sv.org>
2023-09-03 15:48:53 +0300

    add: subabase to profile

--- src/components/resume/ResumeOutline.tsx
+++ src/components/resume/ResumeOutline.tsx
@@ -1,16 +1,40 @@
 import Image from 'next/image'
-import React,{useContext} from 'react'
+import React,{useContext, useEffect, useState } from 'react'
 import { AppContext } from '@/pages/_app'
+import supabase from "@/utils/supabaseClient";
+interface UserProps {
+  profile_picture: string;
+  phone_number: string;
+  full_name: string;
+  email: string;
+  phone: string;
+  address: string;
+}
+  
 
 const ResumeOutline = () => {
     const {state, setState} = useContext(AppContext)
+    const [user, setUser] = useState({} as UserProps);
+
+  useEffect(() => {
+    const getCurUser = async () => {
+      const { data, error } = await supabase
+        .from("user_profile")
+        .select("*")
+        .eq("user_id", (await supabase.auth.getUser()).data.user.id);
+      if (error) return console.log(error);
+      setUser(data[0]);
+    };
+
+    getCurUser();
+  }, []);
   return (
         <div className='flex h-[80vh] w-full rounded-xl border-2'>      
         <div className='bg-[#E6E6E4] w-[50%] flex flex-col gap-3  items-center '>
-            <p className='font-bold text-2xl'>Natenaiel Tadele</p>
-            <p className='font-semibold text-md'>software engineering</p>
+            <p className='font-bold text-2xl'>{user.full_name}</p>
+            <p className='font-semibold text-md capitalize'>software engineering</p>
             <div className='relative w-[5rem] h-[5rem]'>
-                <Image alt='profile image' className=' rounded-full object-cover  w-[9rem] h-[9rem]' fill src='/images/home/team/naty.svg'/>
+                <Image alt='profile image' className=' rounded-full object-cover  w-[9rem] h-[9rem]' fill src={user.profile_picture}/>
             </div>
             <div>
 
@@ -24,12 +48,12 @@ const ResumeOutline = () => {
         <img src="./email.svg" alt="email" />
         <p className="font-semibold">Email</p>
       </span>
-      <p className="text-text-secondary text-sm pl-7 mb-4">naty@email.com</p>
+      <p className="text-text-secondary text-sm pl-7 mb-4">{user.email}</p>
       <span className="flex gap-2">
         <img src="./phone.svg" alt="phone" />
         <p className="font-semibold">Phone Number</p>
       </span>
-      <p className="text-text-secondary text-sm pl-7 mb-4">0917154826</p>
+      <p className="text-text-secondary text-sm pl-7 mb-4">{user.phone_number}</p>
             </div>
 
         </div>
