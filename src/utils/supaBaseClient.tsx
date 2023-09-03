08872bdec272803956d2b1fbeca7663aa30b07f5
Yared04 <yaredt29@gmail.com>
2023-09-03 03:48:25 +0300

    deleted:    src/utils/supabaseClient.tsx

--- src/utils/supaBaseClient.tsx
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://esjytdvpnapaedgiyqhj.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzanl0ZHZwbmFwYWVkZ2l5cWhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM2NDc3NDUsImV4cCI6MjAwOTIyMzc0NX0.OH-oHwJs-w2QbVE86sVbn0e5cD8BySnasM9PtZcDeUI";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
